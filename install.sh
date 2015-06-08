71419ecdaba317aba38be67480029e69364e2c02
auraltension <auraltension@riseup.net>
2015-06-08 19:28:22 +1000

    for now, taking backups is a nice idea :(

--- install.sh
+++ install.sh
@@ -4,27 +4,30 @@
 # Needs some work, have just taken out of ansible playbook
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-MANAGER_DIR='/opt/metanyx/manager/'
+MANAGER_DIR='/opt/metanyx/manager'
+
+rm -rf ${MANAGER_DIR}.bak
+cp -a ${MANAGER_DIR} ${MANAGER_DIR}.bak
 
 # Create manager directory in /opt/metanyx
-mkdir -p ${MANAGER_DIR}{views,static}
-chmod 0700 -R ${MANAGER_DIR}
+mkdir -p ${MANAGER_DIR}/{views,static}
+chmod 0700 -R ${MANAGER_DIR}/
 
 cd ${DIR}/src
-cp bottle.py ${MANAGER_DIR}
-cp manage.py ${MANAGER_DIR}
-chmod 0600 ${MANAGER_DIR}bottle.py
-chmod 0700 ${MANAGER_DIR}manage.py
+cp bottle.py ${MANAGER_DIR}/
+cp manage.py ${MANAGER_DIR}/
+chmod 0600 ${MANAGER_DIR}/bottle.py
+chmod 0700 ${MANAGER_DIR}/manage.py
 
 cp init/metanyx-manager.service /etc/systemd/system/metanyx-manager
 chmod 0600 /etc/systemd/system/metanyx-manager
 cp init/metanyx-manager.sysvinit /etc/init.d/metanyx-manager
 chmod 0700 /etc/init.d/metanyx-manager
 
-cp views/* ${MANAGER_DIR}views/
-chmod 0600 ${MANAGER_DIR}views/*
-cp static/* ${MANAGER_DIR}static/
-chmod 0600 ${MANAGER_DIR}static/*
+cp views/* ${MANAGER_DIR}/views/
+chmod 0600 ${MANAGER_DIR}/views/*
+cp static/* ${MANAGER_DIR}/static/
+chmod 0600 ${MANAGER_DIR}/static/*
 
 systemctl enable metanyx-manager || update-rc.d metanyx-manager defaults
 
